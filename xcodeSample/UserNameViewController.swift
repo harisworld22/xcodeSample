eb8f9e75464b749aacec1e85ea2c95ebdc7dd7ea
Xcode Server <xcodeserver@d-113091461.wipro.com>
2017-09-25 19:48:46 +0530

    Simple UI Testing

---
+++ xcodeSample/UserNameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UserNameViewController.swift
+//  xcodeSample
+//
+//  Created by Xcode Server on 9/25/17.
+//  Copyright © 2017 Xcode Server. All rights reserved.
+//
+
+import UIKit
+
+class UserNameViewController: UIViewController {
+    
+}
--- xcodeSampleUITests/xcodeSampleUITests.swift
+++ xcodeSampleUITests/xcodeSampleUITests.swift
@@ -31,6 +31,11 @@ class xcodeSampleUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        app.textFields["UserName"].tap()
+        app.textFields["UserName"].typeText("Hari")
+        app.textFields["password"].tap()
+        app.textFields["password"].typeText("Hari")
     }
     
 }
